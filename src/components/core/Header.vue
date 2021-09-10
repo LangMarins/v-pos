0f9bc51ca15af616e4a95ac4bd58fbb31405bb0c
langmarins <langmarins@gmail.com>
2021-09-11 01:00:44 +0700

    edit menu + icon

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -1,15 +1,29 @@
 <template>
-  <v-container>
-      <h1>Header page</h1>
-  </v-container>
+  <div>
+    <v-app-bar app color="primary accent-4" dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>JV-POS v {{ version }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <span>username</span>
+      <v-btn color="success" icon @click="onClickLogOff">
+        <v-icon>material-icons logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "Header",
+  computed: {
+    version() {
+      return "0.1.0";
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/core/Menu.vue
+++ src/components/core/Menu.vue
@@ -1,15 +1,37 @@
 <template>
-  <v-container>
-      <h1>Menu page</h1>
-  </v-container>
+  <v-navigation-drawer app absolute permanent>
+    <router-link to="/" exact>
+      <v-img src="@/assets/vue_display.jpg" alt="" width="100%" />
+    </router-link>
+    <v-list shaped>
+      <v-list-item-group v-model="selectedMenu" mandatory color="primary">
+        <v-list-item
+          v-for="([icon, title, route], index) in menus"
+          :key="index"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      selectedMenu: 0,
+      menus: [
+        ["mdi-apps-box", "Stock", "/stock"],
+        ["mdi-chart-areaspline", "Report", "/report"],
+        ["material-icons my_library_books", "About", "/about"],
+      ],
+    };
+  },
+};
 </script>
-
-<style>
-
-</style>
